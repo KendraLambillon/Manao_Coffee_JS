04eb0cb502c7faa52015dc6288d5feb445ff731a
KendraLa <kendralambillon@gmail.com>
2023-09-11 22:58:42 +0200

    Validation Form 1

--- js/validationform.js
+++ js/validationform.js
@@ -13,18 +13,81 @@ sendButton.addEventListener("click", (e)=>{
 })
 
 function validation() {
-    var firstname = document.getElementById("firstname").value;
+    var firstname = document.getElementById("firstname");
+    var lastname = document.getElementById("lastname");
+    var email = document.getElementById("email");
+    var politica = document.getElementById("politica");
 
-    if(firstname == null || firstname == ""){
-        alert("You have to put an input!");
-        return false; //There is an error, so I want "correct" to be false
+
+    let valid = true;
+
+    /******Firstname and Lastname******/
+    //Condition for the firstname
+    if(firstname.value == null || firstname.value == ""){
+        setError(firstname, "You have to put your firstname!");
+        valid = false; //There is an error, so I want "valid" to be false
+    }else{
+        //Regular expression
+        let name_re = /^[a-zA-Z ]{2,30}$/;
+
+        //To validate the regular expression
+        if(!name_re.exec(firstname.value)){
+            setError(firstname, "Firstname invalid");
+            valid = false;
+        }else{
+            setSuccess(firstname);
+        }
     }
 
-    let name_re = /^[a-zA-Z ]{2,30}$/ //Regular expression
-    //To validate the regular expression
-    if(!name_re.exec(firstname)){
-        alert("Firstname invalid");
+    //Condition for the lastname
+    if(lastname.value == null || lastname.value == ""){
+        setError(lastname, "You have to put your lastname!");
+        valid = false; //There is an error, so I want "valid" to be false
+    }else{
+        //To validate the regular expression
+        if(!name_re.exec(lastname.value)){
+            setError(lastname, "Lastname invalid");
+            valid = false;
+        }else{
+            setSuccess(lastname);
+        }
+    }
+
+    /******Email******/
+    if(email.value == null || email.value == "") {
+        setError(email, "You have to put your email!");
+        valid = false;
+    }else{
+        setSuccess(email);
+    }
+
+    /******Privacity and politic******/
+    if(!politica.checked) {
+        setError(politica, "You have to accept the privacity.");
+        valid = false;
+    }else{
+        setSuccess(politica);
+    }
+
+    if(valid == true) {
+        return true;
+    }else{
+
         return false;
     }
-    return true; //If there is no error and the user has put an input
+}
+
+
+//Error message
+function setError(input, message){
+    const formItem = input.parentElement;
+    const small = formItem.querySelector("small");
+    formItem.className = "form-item error";
+    small.innerText = message;
+}
+
+//If everything is correct
+function setSuccess(input){
+    const formItem = input.parentElement;
+    formItem.className = "form-item success";
 }
---
+++ js/validationform1.js
@@ -0,0 +1,102 @@
+const form = document.getElementById("form");
+
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+
+let validate = {
+    firstname: false,
+    lastname: false,
+    phone: false,
+    email: false
+}
+
+//Error message
+function setError(input, message){
+    const formItem = input.parentElement;
+    const small = formItem.querySelector("small");
+    formItem.className = "form-item error";
+    small.innerText = message;
+}
+
+//If everything is correct
+function setSuccess(input){
+    const formItem = input.parentElement;
+    formItem.className = "form-item success";
+}
+
+
+//Validation for the firstname
+firstname.addEventListener("blur",()=>{
+    let firstname_re = /^[a-zA-Z ]{2,30}$/;
+
+    if(firstname.value == "" || firstname.value == null){
+        validate.firstname = false;
+        setError(firstname, "Put your firstname!");
+    }else{
+        if(!firstname_re.exec(firstname.value)){
+            validate.firstname = false;
+            setError(firstname, "Firstname has to be between 2-30 characters.")
+        }else{
+            validate.firstname = true;
+            setSuccess(firstname);
+        }
+    }
+})
+
+//Validation for the lastname
+lastname.addEventListener("blur",()=>{
+    let lastname_re = /^[a-zA-Z ]{2,30}$/;
+
+    if(lastname.value == "" || lastname.value == null){
+        validate.lastname = false;
+        setError(lastname, "Put your lastname!");
+    }else{
+        if(!lastname_re.exec(lastname.value)){
+            validate.lastname = false;
+            setError(lastname, "Lastname has to be between 2-30 characters.")
+        }else{
+            validate.lastname = true;
+            setSuccess(lastname);
+        }
+    }
+})
+
+//Validation for the phone
+phone.addEventListener("blur",()=>{
+    let phone_re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3}$/;
+
+    if(phone.value == "" || phone.value == null){
+        validate.phone = false;
+        setError(phone, "Put your phone!");
+    }else{
+        if(!phone_re.exec(phone.value)){
+            validate.phone = false;
+            setError(phone, "Phone has to have 9 numbers.")
+        }else{
+            validate.phone = true;
+            setSuccess(phone);
+        }
+    }
+})
+
+//Validation for the email
+email.addEventListener("blur",()=>{
+    let email_re = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+    if(email.value == "" || email.value == null){
+        validate.email = false;
+        setError(email, "Put your email!");
+    }else{
+        if(!email_re.exec(email.value)){
+            validate.email = false;
+            setError(email, "Email is not valid!")
+        }else{
+            validate.email = true;
+            setSuccess(email);
+        }
+    }
+})
+
+
